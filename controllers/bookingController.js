7f77e78543af10eea5d10c6c12e4899bcc5dcf99
VeD Ýuq <rongthepfc107@gmail.com>
2024-05-19 17:31:50 +0700

    update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,40 +1,45 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const Tour = require('../models/tourModel');
+const { v4: uuidv4 } = require('uuid');
+const Hotel = require('../models/hotelModel');
 const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1) Get the currently booked tour
-  const tour = await Tour.findById(req.params.tourId);
-  // console.log(tour);
+  const hotel = await Hotel.findById(req.params.hotelId);
+  if (!hotel) {
+    return next(new AppError('No hotel found with that ID', 404));
+  }
 
-  // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-bookings?alert=booking`,
+    cancel_url: `${req.protocol}://${req.get('host')}/hotel/${hotel.slug}`,
     customer_email: req.user.email,
-    client_reference_id: req.params.tourId,
+    client_reference_id: req.params.hotelId,
     line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
+        name: `${hotel.name} Room`,
+        description: `Room ${req.body.roomID} at ${hotel.name}`,
         images: [
-          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+          `${req.protocol}://${req.get('host')}/img/hotels/${hotel.imageCover}`,
         ],
-        amount: tour.price * 100,
+        amount: req.body.price * 100,
         currency: 'usd',
         quantity: 1,
       },
     ],
+    metadata: {
+      checkIn: req.body.checkIn,
+      checkOut: req.body.checkOut,
+      room: req.body.room,
+      adult: req.body.adult,
+      child: req.body.child,
+    },
   });
 
-  // 3) Create session as response
   res.status(200).json({
     status: 'success',
     session,
@@ -42,10 +47,21 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
+  const hotel = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
-  await Booking.create({ tour, user, price });
+  const price = session.amount_total / 100;
+  await Booking.create({
+    hotel,
+    user,
+    price,
+    bookingCode: uuidv4().slice(0, 6).toUpperCase(),
+    checkIn: session.metadata.checkIn,
+    checkOut: session.metadata.checkOut,
+    room: session.metadata.room,
+    adult: session.metadata.adult,
+    child: session.metadata.child,
+    totalPrice: session.amount_total / 100,
+  });
 };
 
 exports.webhookCheckout = (req, res, next) => {
@@ -68,8 +84,37 @@ exports.webhookCheckout = (req, res, next) => {
   res.status(200).json({ received: true });
 };
 
-exports.createBooking = factory.createOne(Booking);
+exports.createBooking = catchAsync(async (req, res, next) => {
+  if (!req.body.bookingCode) {
+    req.body.bookingCode = uuidv4().slice(0, 6).toUpperCase();
+  }
+
+  const newBooking = await Booking.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      booking: newBooking,
+    },
+  });
+});
+
 exports.getBooking = factory.getOne(Booking);
 exports.getAllBookings = factory.getAll(Booking);
 exports.updateBooking = factory.updateOne(Booking);
 exports.deleteBooking = factory.deleteOne(Booking);
+
+exports.getBookingByCode = catchAsync(async (req, res, next) => {
+  const booking = await Booking.findOne({ bookingCode: req.params.code });
+
+  if (!booking) {
+    return next(new AppError('No booking found with that code', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      booking,
+    },
+  });
+});
--- models/bookingModel.js
+++ models/bookingModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const bookingSchema = new mongoose.Schema({
   bookingDate: {
@@ -18,6 +19,63 @@ const bookingSchema = new mongoose.Schema({
     ref: 'Client',
     required: [true, 'A booking must have a client'],
   },
+  bookingCode: {
+    type: String,
+    required: [true, 'A booking must have a booking code'],
+  },
+  adult: {
+    type: Number,
+    required: [true, 'A booking must specify number of adults'],
+  },
+  child: {
+    type: Number,
+    required: [true, 'A booking must specify number of children'],
+  },
+  checkIn: {
+    type: Date,
+    required: [true, 'A booking must have a check-in date'],
+  },
+  checkOut: {
+    type: Date,
+    required: [true, 'A booking must have a check-out date'],
+  },
+  hotel: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Hotel',
+    required: [true, 'A booking must have a hotel'],
+  },
+  packageId: {
+    type: Number,
+    required: [true, 'A booking must have a package ID'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'A booking must have a price'],
+  },
+  room: {
+    type: Number,
+    required: [true, 'A booking must have a room'],
+  },
+  roomID: {
+    type: String,
+    required: [true, 'A booking must have a room ID'],
+  },
+  services: {
+    type: [String],
+    required: [true, 'A booking must have services'],
+  },
+  totalPrice: {
+    type: Number,
+    required: [true, 'A booking must have a total price'],
+  },
+});
+
+// Add pre-save middleware to generate bookingCode
+bookingSchema.pre('save', function (next) {
+  if (!this.bookingCode) {
+    this.bookingCode = uuidv4().slice(0, 6).toUpperCase();
+  }
+  next();
 });
 
 const Booking = mongoose.model('Booking', bookingSchema);
