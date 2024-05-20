d546749309cc55ad2335056311d91b4ff5e4ef0d
VeD Ýuq <rongthepfc107@gmail.com>
2024-05-21 01:33:33 +0700

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,9 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    secure:
+      req.headers['x-forwarded-proto'] === 'https' ||
+      process.env.NODE_ENV === 'production',
   });
 
   // Remove password from output
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -10,16 +16,29 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
-  // .connect(DB.env.DATABASE_LOCAL, {
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful!'));
 
 const port = 3000 || process.env.PORT;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}...`);
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
 });
